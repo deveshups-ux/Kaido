ee4948548a803864d70d5e54f89dc067abb87447
DEVESH TIWARI <deveshups@gmail.com>
2026-08-22 12:04:57 +0530

    Add Google login UI and organize frontend utilities

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,10 @@
-import { signInWithPopup } from "firebase/auth";
-import { auth, googleProvider } from "../utils/firebase.js";
-import React from "react";
-import api from "../utils/axios.js";
+import Home from "./pages/Home";
 
 const App = () => {
-  const handleLogin = async (token) => {
-    try {
-      const { data } = await api.post("/auth/login", { token });
-      console.log(data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const googleLogin = async () => {
-    const data = await signInWithPopup(auth, googleProvider);
-    const token = await data.user.getIdToken();
-    console.log(token);
-    await handleLogin(token);
-    console.log(data);
-  };
   return (
-    <div className="w-full h-screen bg-black flex items-center justify-center">
-      <button
-        onClick={googleLogin}
-        className="bg-white text-black py-2 px-4 rounded-md hover:bg-gray-200"
-      >
-        continue with google
-      </button>
-    </div>
+    <>
+      <Home />
+    </>
   );
 };
 
