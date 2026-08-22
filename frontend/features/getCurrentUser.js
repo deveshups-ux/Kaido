b713b8f6ad7fbd9172cce394438e4b8b5455b288
DEVESH TIWARI <deveshups@gmail.com>
2026-08-22 17:16:29 +0530

    some bugs

--- frontend/features/getCurrentUser.js
+++ frontend/features/getCurrentUser.js
@@ -1,11 +1,13 @@
-import api from "../src/utils/axios.js";
+import api from "../src/utils/axios";
 
 const getCurrentUser = async () => {
   try {
     const { data } = await api.get("/api/me");
     console.log(data);
+    return data;
   } catch (error) {
     console.error("Error fetching current user:", error);
+    return null;
   }
 };
 
