7347aafb90a097db4f24e75b5c6d081965b614f9
DEVESH TIWARI <deveshups@gmail.com>
2026-08-28 19:50:00 +0530

    frontend part

--- backend/gateway/utils/proxyWithHeader.js
+++ backend/gateway/utils/proxyWithHeader.js
@@ -6,6 +6,7 @@ export const proxyWithHeader = (secureUrl) => {
       if (srcReq.user) {
         proxyReqOpts.headers["x-user-id"] = srcReq.user.userId;
       }
+      return proxyReqOpts;
     },
   });
 };
